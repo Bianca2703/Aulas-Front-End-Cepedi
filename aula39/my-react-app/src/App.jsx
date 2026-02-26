22f8e2400e7f1c5c0547d470465e25da22e813f6
Gustavo Dos Reis Rodrigues <0116998@ifmg.edu.br>
2026-02-26 16:19:42 -0300

    Adiciona pasta aula39

---
+++ aula39/my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => setPosts(data));
+  },[]);
+
+  return (
+<ul>
+  {posts.map(post => (
+    <li key={post.id}>{post.title}</li>
+  ))}
+</ul>
+  )
+}
+
+export default App
